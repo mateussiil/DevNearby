d9a24311934a0690bd71e599baf18b442486691c
Mateus Silva <mateus_silva97@hotmail.com>
2023-01-11 18:36:55 -0300

    style: fixed color

--- mobile/App.js
+++ mobile/App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <>
       <StatusBar 
-        barStyle="light-content" backgroundColor="#7D40E7" />
+        barStyle="light-content" backgroundColor="#013959" />
       <Routes />
     </>
   );
